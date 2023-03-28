27ad6c86977ea958864ae3e7a51b2686cb4a498e
nasadkina <kasatkasasha@gmail.com>
2023-03-28 21:24:20 +0300

    solution for prime numbers updated

--- homework_01/main.py
+++ homework_01/main.py
@@ -1,10 +1,13 @@
+import math
+
 
 def power_numbers(numbers):
     return [number ** 2 for number in numbers]
 
+
 # filter types
-numbers = [1, 2, 3, 4]
-print(power_numbers(numbers))
+#numbers = [1, 2, 3, 4]
+#print(power_numbers(numbers))
 
 ODD = "odd"
 EVEN = "even"
@@ -12,18 +15,17 @@ PRIME = "prime"
 
 
 def is_prime(number):
-    for i in range(2, number):
+    for i in range(2, int(math.sqrt(number)) + 1):
         if (number % i) == 0:
             return False
-        return True
+    return True
 
 
-numbers_list = [1, 2, 3, 4, 5, 6, 7, 8]
-filter_type = PRIME
+#numbers_list = [1, 2, 3, 4, 5, 6, 7, 8, 11]
+#filter_type = PRIME
 
 
 def filter_numbers(numbers_list, filter_type):
-
     if filter_type == ODD:
         return [number for number in numbers_list if number % 2 != 0]
     if filter_type == EVEN:
@@ -32,10 +34,4 @@ def filter_numbers(numbers_list, filter_type):
         return [number for number in numbers_list if (is_prime(number) is True)]
 
 
-print(filter_numbers(numbers_list, filter_type))
-
-
-
-
-
-
+#print(filter_numbers(numbers_list, filter_type))
