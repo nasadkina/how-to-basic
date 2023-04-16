e733094b91ea314b15763e9f0464a4b693e32988
nasadkina <kasatkasasha@gmail.com>
2023-04-16 18:04:40 +0300

    add homework_02

--- homework_01/main.py
+++ homework_01/main.py
@@ -1,33 +1,33 @@
-"""
-Домашнее задание №1
-Функции и структуры данных
-"""
+import math
 
 
 def power_numbers(*numbers):
-    """
-    функция, которая принимает N целых чисел,
-    и возвращает список квадратов этих чисел
-    >>> power_numbers(1, 2, 5, 7)
-    <<< [1, 4, 25, 49]
-    """
     return [number ** 2 for number in numbers]
 
 
 # filter types
+#numbers = [1, 2, 3, 4]
+#print(power_numbers(numbers))
+
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
-    """
-    функция, которая на вход принимает список из целых чисел,
-    и возвращает только чётные/нечётные/простые числа
-    (выбор производится передачей дополнительного аргумента)
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, int(math.sqrt(number)) + 1):
+        if (number % i) == 0:
+            return False
+    return True
 
-    >>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
-    >>> filter_numbers([2, 3, 4, 5], EVEN)
-    <<< [2, 4]
-    """
+
+
+def filter_numbers(numbers_list, filter_type):
+    if filter_type == ODD:
+        return [number for number in numbers_list if number % 2 != 0]
+    if filter_type == EVEN:
+        return [number for number in numbers_list if number % 2 == 0]
+    if filter_type == PRIME:
+        return [number for number in numbers_list if (is_prime(number) is True)]
