8030840e299772d71fea266a816d750ea7a61a51
nasadkina <kasatkasasha@gmail.com>
2023-06-14 21:04:36 +0300

    added meme page

--- homework_05/app.py
+++ homework_05/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -9,9 +11,18 @@ def index():
     return render_template('base.html')
 
 
+def get_meme():
+    url = "https://meme-api.com/gimme/wholesomememes"
+    response = json.loads(requests.request("GET", url).text)
+    meme_large = response["preview"][-2]
+    subreddit = response["subreddit"]
+    return meme_large, subreddit
+
+
 @app.get("/about/")
 def about():
-    return render_template('base.html')
+    meme_pic, subreddit = get_meme()
+    return render_template('meme.html', meme_pic=meme_pic, subreddit=subreddit)
 
 
 if __name__ == '__main__':
